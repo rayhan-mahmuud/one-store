05439c9ae008adb443a0d517f1960f97ca8e72e3
Rayhan Mahmud <rayhan.mahmuud@gmail.com>
2025-03-05 23:02:22 +0600

    Final fixes for auth config

---
+++ auth.config.ts
@@ -0,0 +1,40 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "./db/prisma";
+import { compare } from "./lib/encrypt";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        const user = await prisma.user.findFirst({
+          where: { email: credentials.email as string },
+        });
+
+        if (user && user.password) {
+          const isMatch = await compare(
+            credentials.password as string,
+            user.password
+          );
+
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -2,10 +2,9 @@ import NextAuth from "next-auth";
 import type { NextAuthConfig } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./db/prisma";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { compare } from "./lib/encrypt";
+import authConfig from "./auth.config";
 
 export const config = {
   pages: {
@@ -20,39 +19,7 @@ export const config = {
 
   adapter: PrismaAdapter(prisma),
 
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { type: "email" },
-        password: { type: "password" },
-      },
-      async authorize(credentials) {
-        if (credentials == null) return null;
-
-        const user = await prisma.user.findFirst({
-          where: { email: credentials.email as string },
-        });
-
-        if (user && user.password) {
-          const isMatch = await compare(
-            credentials.password as string,
-            user.password
-          );
-
-          if (isMatch) {
-            return {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-              role: user.role,
-            };
-          }
-        }
-        return null;
-      },
-    }),
-  ],
+  ...authConfig,
   callbacks: {
     async session({ session, user, trigger, token }: any) {
       session.user.id = token.sub;
